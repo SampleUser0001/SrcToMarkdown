cfb224527e666e240e73480b0155ff4dfd0f0961
SampleUser0001 <ittimfn+github@gmail.com>
2023-03-20 11:21:41 +0900

    エクスポートディレクトリを絶対パスで扱うように修正

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,13 @@
 require('date-utils');
+const path = require('path');
 
-const EXPORT_DIR = '../export';
+const EXPORT_DIR_NAME = 'export';
 
 const getExportPath = (date) => {
-    const filename = date.toFormat('YYYYMMDDHH24MISS');
-    return EXPORT_DIR + "/" + filename + ".md";
+    const filename = date.toFormat('YYYYMMDDHH24MISS') + ".md";
+    const projectRoot = path.join(__dirname, "..");
+    const exportRoot = path.join(projectRoot, EXPORT_DIR_NAME)
+    return path.join(exportRoot, filename);
 }
 
 module.exports.getExportPath = getExportPath
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,9 +1,12 @@
 const utils = require('./utils');
+const path = require('path');
 
 test('getExportPathが引数から年月日時分秒のパスを返すこと', () => {
     // 2023年3月18日 23時45分01秒 -> 20230318234501
     const testDate = new Date(
         2023, 2, 18, 23, 45, 1
     );
-    expect(utils.getExportPath(testDate)).toBe('../export/20230318234501.md');
+    const projectRoot = path.join(__dirname, "..");
+    const expectDir = path.join(projectRoot, 'export', '20230318234501.md');
+    expect(utils.getExportPath(testDate)).toBe(expectDir);
 });
